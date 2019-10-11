522b3860299067ed64e35ae2b46d40c22912a353
Warren Day <warrenjday@gmail.com>
2019-10-11 18:08:50 +0100

    Updates

---
+++ precache-manifest.c091b54186d4cdcbef378e02fadbe0dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1272168eace85e1035879ec5031905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e17b8a2d1d76afddd40",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "8e17b8a2d1d76afddd40",
+    "url": "/static/js/2.c47d2dd9.chunk.js"
+  },
+  {
+    "revision": "4fcd22179928fac1dd8f",
+    "url": "/static/js/main.c83be27b.chunk.js"
+  },
+  {
+    "revision": "0eb5ccc42bd14dfebdaf",
+    "url": "/static/js/runtime-main.16f30a8e.js"
+  }
+]);
