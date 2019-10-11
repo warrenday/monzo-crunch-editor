cf5d8ac538126351e30012e40cb0196f764074a1
Warren Day <warrenjday@gmail.com>
2019-10-11 17:52:53 +0100

    Updates

---
+++ precache-manifest.282cb79d00f771a19d318705afc1898a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df1bf8e4f35ba50c83bd5bf85211ef9",
+    "url": "/warrenday/monzo-crunch-editor/index.html"
+  },
+  {
+    "revision": "1a8d990b4e2174a09dad",
+    "url": "/warrenday/monzo-crunch-editor/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "1a8d990b4e2174a09dad",
+    "url": "/warrenday/monzo-crunch-editor/static/js/2.6a454860.chunk.js"
+  },
+  {
+    "revision": "68df597a18d327c4a4b3",
+    "url": "/warrenday/monzo-crunch-editor/static/js/main.c9cf697c.chunk.js"
+  },
+  {
+    "revision": "2b1047cec8fbe798f123",
+    "url": "/warrenday/monzo-crunch-editor/static/js/runtime-main.8fb38f16.js"
+  }
+]);
